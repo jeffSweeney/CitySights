b71515b80603f6d2f94e9cd0e756cbf3ffdff0a1
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-12-23 13:50:08 -0500

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Jeffrey Sweeney on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
