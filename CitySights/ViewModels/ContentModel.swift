f320e579581b545e9747345ca32237fae0780459
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-12-23 16:07:22 -0500

    - Get permission to geo locate user in ContentModel - Conform ContentModel to CLLocationManagerDelegate and define delegate methods to handle location input

--- CitySights/ViewModels/ContentModel.swift
+++ CitySights/ViewModels/ContentModel.swift
@@ -6,7 +6,40 @@
 //
 
 import Foundation
+import CoreLocation
 
-class ContentModel: ObservableObject {
+class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject {
     
+    var locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        
+        // Assign location manager delegate to self for handling permission selections
+        locationManager.delegate = self
+        
+        // Request permission from the user to geo locate
+        locationManager.requestWhenInUseAuthorization()
+
+    }
+    
+    // MARK: - Location Manager Delegate Methods
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let authStatus = locationManager.authorizationStatus
+        
+        if authStatus == .authorizedAlways || authStatus == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
+        else if authStatus == .denied {
+            // TODO: User MUST authorize permission for app to work - enforce
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Capture locations
+        // TODO: Send location coordintes off to the Yelp API
+        
+        // Only need location once - stop requesting
+        locationManager.stopUpdatingLocation()
+    }
 }
