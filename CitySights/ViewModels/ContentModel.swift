824579db9f12476a73ba6baa2f7efeabfcc9f249
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-12-26 12:28:36 -0500

    Build network requests to retrieve data from the Yelp API

--- CitySights/ViewModels/ContentModel.swift
+++ CitySights/ViewModels/ContentModel.swift
@@ -36,10 +36,56 @@ class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // Capture locations
-        // TODO: Send location coordintes off to the Yelp API
+        // Capture locations and send off the the Yelp API
+        if let userLocation = locations.first {
+            // Retrieve restaurants
+            getBusinesses(Constants.Restaurants, userLocation)
+            
+            // Retrieve arts
+//            let arts = getBusinesses(Constants.ArtsAndEntertainment, userLocation)
+            
+            // Only need location once - stop requesting
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    // MARK: - Yelp API Methods
+    func getBusinesses(_ category: String, _ userLocation: CLLocation) {
+        // Create URL
+        var urlComponents = URLComponents(string: Constants.YelpApiUrl)
+        urlComponents?.queryItems = [
+            URLQueryItem(name: Constants.Latitude, value: String(userLocation.coordinate.latitude)),
+            URLQueryItem(name: Constants.Longitude, value: String(userLocation.coordinate.longitude)),
+            URLQueryItem(name: Constants.Categories, value: category),
+            URLQueryItem(name: Constants.Limit, value: Constants.DefaultLimit)
+        ]
         
-        // Only need location once - stop requesting
-        locationManager.stopUpdatingLocation()
+        if let url = urlComponents?.url {
+            // Create URL Request
+            var request =  URLRequest(
+                url: url,
+                cachePolicy: .reloadIgnoringLocalCacheData,
+                timeoutInterval: 10.0
+            )
+            
+            // Set HTTP Header
+            request.httpMethod = Constants.HTTPMethod
+            request.addValue(Constants.HTTPAuthorization, forHTTPHeaderField: "Authorization")
+            
+            // Get URL Session
+            let session = URLSession.shared
+            
+            // Create Data Task
+            let task = session.dataTask(with: request) { (data, response, error) in
+                if error == nil {
+                    print(response!)
+                } else {
+                    print(error!)
+                }
+            }
+            
+            // Launch Data Task
+            task.resume()
+        }
     }
 }
