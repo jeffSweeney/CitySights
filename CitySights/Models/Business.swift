a5cc5efef14080cfc41a8d1626b7f694b9d6ee95
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-12-28 10:05:04 -0500

    Use camelCase by defining coding keys

--- CitySights/Models/Business.swift
+++ CitySights/Models/Business.swift
@@ -9,14 +9,14 @@ import Foundation
 
 struct Business: Decodable, Identifiable {
     var id: String?
-    var is_closed: Bool?
-    var review_count: Int?
+    var isClosed: Bool?
+    var reviewCount: Int?
     var categories: [Category]?
     var rating: Double?
-    var image_url: String?
+    var imageUrl: String?
     var url: String?
     var phone: String?
-    var display_phone: String?
+    var displayPhone: String?
     var price: String?
     var location: Location?
     var alias: String?
@@ -24,6 +24,28 @@ struct Business: Decodable, Identifiable {
     var transactions: [String]?
     var distance: Double?
     var name: String?
+    
+    enum CodingKeys: String, CodingKey {
+        // Mapped key changes
+        case isClosed = "is_closed"
+        case reviewCount = "review_count"
+        case imageUrl = "image_url"
+        case displayPhone = "display_phone"
+        
+        // Remainder of non-changed keys
+        case id
+        case categories
+        case rating
+        case url
+        case phone
+        case price
+        case location
+        case alias
+        case coordinates
+        case transactions
+        case distance
+        case name
+    }
 }
 
 struct Category: Decodable {
@@ -32,12 +54,26 @@ struct Category: Decodable {
 }
 
 struct Location: Decodable {
-    var display_address: [String]?
+    var displayAddress: [String]?
     var city: String?
     var address1: String?
     var address2: String?
     var address3: String?
-    var zip_code: String?
+    var zipCode: String?
     var country: String?
     var state: String?
+    
+    enum CodingKeys: String, CodingKey {
+        // Mapped key changes
+        case displayAddress = "display_address"
+        case zipCode = "zip_code"
+        
+        // Remainder of non-changed keys
+        case city
+        case address1
+        case address2
+        case address3
+        case country
+        case state
+    }
 }
