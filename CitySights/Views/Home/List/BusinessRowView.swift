e9d47b87be98654558e662f4385cbf7852b424a4
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-12-28 11:01:56 -0500

    Build the business row view

---
+++ CitySights/Views/Home/List/BusinessRowView.swift
@@ -0,0 +1,44 @@
+//
+//  BusinessRowView.swift
+//  CitySights
+//
+//  Created by Jeffrey Sweeney on 12/28/21.
+//
+
+import SwiftUI
+
+struct BusinessRowView: View {
+    // MUST be observed since the business row may have to wait a moment
+    // for the image to be retrieved.
+    @ObservedObject var business: Business
+    
+    var body: some View {
+        VStack {
+            HStack {
+                let uiImage = UIImage(data: business.imageData ?? Data())
+                Image(uiImage: uiImage ?? UIImage())
+                    .resizable()
+                    .frame(width: 48, height: 48)
+                    .cornerRadius(15.0)
+                    .scaledToFit()
+                
+                VStack (alignment: .leading) {
+                    Text(business.name ?? "Name Unavailable")
+                    Text("\(business.distanceAsMiles()) mi. away")
+                        .font(.caption)
+                }
+                
+                Spacer()
+                
+                VStack (alignment: .leading) {
+                    Image("regular_\(business.rating ?? 0.0)")
+                    
+                    Text("\(business.reviewCount ?? 0) reviews")
+                        .font(.caption)
+                }
+            }
+            
+            Divider()
+        }
+    }
+}
