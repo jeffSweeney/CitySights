abc870065ea74baecee795aa8905eff26d5ef93a
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2022-01-09 11:44:14 -0500

    Boiler plate for new DirectionsView

---
+++ CitySights/Views/Home/Detail/DirectionsView.swift
@@ -0,0 +1,23 @@
+//
+//  DirectionsView.swift
+//  CitySights
+//
+//  Created by Jeffrey Sweeney on 1/9/22.
+//
+
+import SwiftUI
+
+struct DirectionsView: View {
+    
+    var business: Business
+    
+    var body: some View {
+        
+        VStack (alignment: .leading) {
+            // Business Title
+            
+            
+            // Directions Map
+        }
+    }
+}
