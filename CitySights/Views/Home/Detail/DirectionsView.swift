6d5562792c093a22daf435e4dc48547d04798ec9
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2022-01-09 14:07:41 -0500

    Build out DirectionsView to contain the BusinessTitleView and DirectionsMap below

--- CitySights/Views/Home/Detail/DirectionsView.swift
+++ CitySights/Views/Home/Detail/DirectionsView.swift
@@ -9,15 +9,33 @@ import SwiftUI
 
 struct DirectionsView: View {
     
+    @EnvironmentObject var contentModel: ContentModel
+    
     var business: Business
     
     var body: some View {
         
         VStack (alignment: .leading) {
             // Business Title
-            
+            HStack {
+                BusinessTitleView(business: business)
+                
+                Spacer()
+                
+                if let url = contentModel.getMapLink(business: business) {
+                    Link("Open in Maps", destination: url)
+                        .padding(.trailing, 10)
+                } else {
+                    Text("Directions Unavailable")
+                        .bold()
+                        .foregroundColor(.red)
+                        .padding(.trailing, 10)
+                }
+            }
             
             // Directions Map
+            DirectionsMap(business: business)
+                .ignoresSafeArea(edges: .bottom)
         }
     }
 }
