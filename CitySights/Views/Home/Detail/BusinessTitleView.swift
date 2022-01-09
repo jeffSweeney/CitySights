246e0b6a5c14b38f46931d075ce54073166d3947
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2022-01-09 11:43:16 -0500

    Build reusable business title view

---
+++ CitySights/Views/Home/Detail/BusinessTitleView.swift
@@ -0,0 +1,36 @@
+//
+//  BusinessTitleView.swift
+//  CitySights
+//
+//  Created by Jeffrey Sweeney on 1/9/22.
+//
+
+import SwiftUI
+
+struct BusinessTitleView: View {
+    var business: Business
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            // Fixed size prevents long business names from abbreviating
+            Text(business.name ?? "Name Unavailable")
+                .font(.largeTitle)
+                .padding()
+                .fixedSize(horizontal: false, vertical: true)
+            
+            if let displayAddress = business.location?.displayAddress {
+                ForEach(displayAddress, id: \.self) { line in
+                    Text(line)
+                        .bold()
+                        .foregroundColor(.gray)
+                        .padding(.horizontal)
+                }
+            }
+            
+            if let rating = business.rating {
+                Image("regular_\(rating)")
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
